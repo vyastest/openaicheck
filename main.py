442cde9c21f0c7488ed8fbb266dbf3ca7e66830e
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 15:24:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import openai
 import streamlit as st
 
-st.title("Streamlit Chat Interface")
+# Add sidebar content
+st.sidebar.title("ChatGPT-like clone")
+st.sidebar.markdown("This app demonstrates a ChatGPT-like clone using Streamlit and the OpenAI API.")
+st.sidebar.markdown("Enter your OpenAI API Key in the input field on the sidebar to get started.")
 
-api_key = st.text_input("Enter your OpenAI API Key", type="password")
+# Request API Key in the sidebar
+api_key = st.sidebar.text_input("Enter your OpenAI API Key", type="password")
+
+# Set API Key
 openai.api_key = api_key
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
