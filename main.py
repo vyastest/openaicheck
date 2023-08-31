8def09f67f4e7e0930c4c45b0365d708a390cb17
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-31 16:44:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ st.sidebar.title("ChatGPT-")
 # Request API Key in the sidebar
 #api_key = st.sidebar.text_input("Enter your OpenAI API Key", type="password")
 
-import openai
 openai.api_key = "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"
 
 prompt = "the massage was really good and reception guys sunil and imran very polite and they understand what customer requirements are my back was paining so they suggested me to take aroma massage it was relaxing.  massage was amazing super..Please summarise the sentence in 8 words"
